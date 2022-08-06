0c45193479d1dcf39a73a47ef86a10bd0fee5a15
techx1 <ss.uitech01@gmail.com>
2022-08-06 16:52:16 +0500

    solved blur issue

--- pages/form/fill-form.jsx
+++ pages/form/fill-form.jsx
@@ -61,7 +61,7 @@ const FillForm = () => {
             )
           )}
           <button
-            onMouseDown={onSubmit}
+            onMouseUp={onSubmit}
             className="text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
           >
             Submit
